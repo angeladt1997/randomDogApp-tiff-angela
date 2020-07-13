335bb4ac8aee0f04bd84a46ba0ab8503e2175950
Tiffany Summerford <tiffanysummerford@gmail.com>
2020-07-13 16:25:47 -0400

    working again

--- index.js
+++ index.js
@@ -7,18 +7,12 @@ function getDogImage() {
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
-//for of loop thru responseJson.message
 
 function displayResults(responseJson) {
-  console.log(responseJson.message);
-  //replace the existing image with the new one
+  console.log(responseJson);
   for (let i = 0; i < responseJson.message.length; i++){
-  $('.results').append(`<img src="${responseJson.message[i]}" class="results-img">`)
+    $('.results').append(`<img src="${responseJson.message[i]}" class="results-img">`)
   }
-  // $('.results-img').replaceWith(
-  //   `<img src="${responseJson.message[]}" class="results-img">`
-  // )
-  //display the results section
   $('.results').removeClass('hidden');
 }
 
@@ -29,20 +23,7 @@ function watchForm() {
   });
 }
 
-// function fetchNumber(){
-//   let num = null; //or #if that doesn't work
-//   console.log("working")
-// }
-
-//create data store arr
-//create variable for number in fetch
-//create function that takes in fetchNumber and outputs fetchNumber pictures
-
-
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
-
-//The then() method returns a Promise. It takes up to two arguments:
-//callback functions for the success & failure cases of the Promise.
