f291bef5b7da030590498c50edf7d20b9e492b2b
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-07-13 23:20:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-'use strict';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dog API Example</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Dog API: A Simple Example</h1>
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random/3')
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
-}
-
-function displayResults(responseJson) {
-  console.log(responseJson);
-  for (let i = 0; i < responseJson.message.length; i++){
-    $('.results').append(`<img src="${responseJson.message[i]}" class="results-img">`)
-  }
-  $('.results').removeClass('hidden');
-}
-
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getDogImage();
-  });
-}
-
-$(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
-});
+        <form>
+          <input id="textArea" type="text" value="" placeholder="Type in a number!" required>
+          <input type="submit" value="Get a dog pic!">
+          <button type="button" class="reset-button">Get more pictures!</button>
+        </form>
+      
+        
+        <section class="results hidden">
+          <h2>Look at this dog!</h2>
+        </section>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="index.js"></script>
+</body>
+</html>
